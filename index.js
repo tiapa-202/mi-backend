f2e2fc8be696c985ea70a4b09ea57618c7bc254f
tiapa-202 <tiapadiego16@gmail.com>
2026-08-18 16:02:32 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+// Usar body-parser para entender datos JSON
+app.use(bodyParser.json());
+
+// Ruta GET (para probar que el servidor funciona)
+app.get('/', (req, res) => {
+    res.json({ mensaje: '¡Hola! Soy tu backend', fecha: new Date() });
+});
+
+// Ruta POST: RECIBIR TAREAS DEL FRONTEND
+app.post('/tareas', (req, res) => {
+    const tarea = req.body;
+    console.log('📥Tarea recibida:', tarea);
+
+    res.json({
+        mensaje: 'Tarea recibida correctamente',
+        tareaRecibida: tarea,
+        fecha: new Date()
+    });
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(` Servidor corriendo en http://localhost:${PORT}`);
+});
