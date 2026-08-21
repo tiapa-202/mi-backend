bfd7685fb5a0eb76c27f6faae4ebc8a1de03204a
tiapa-202 <tiapadiego16@gmail.com>
2026-08-21 19:34:40 -0400

    actu

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ app.use(bodyParser.json());
 // CONEXIÓN A POSTGRESQL (usando variables de entorno)
 // ============================================
 const pool = new Pool({
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
