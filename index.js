3ef0e96f5e62883333923fc86302668c67c4d96d
tiapa-202 <tiapadiego16@gmail.com>
2026-08-29 12:07:12 -0400

    actu

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 // ============================================
 // CONFIGURACIÓN CORS: LA CLAVE ESTÁ AQUÍ
